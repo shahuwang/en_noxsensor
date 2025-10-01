35bd9306ec907d40e65e6ebab30a89b017a17bc7
shahuwang <shahuwang@kugou.net>
2025-10-02 04:52:24 +0800

    update

--- public/js/main.js
+++ public/js/main.js
@@ -556,8 +556,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const faviconDarkExists = "false" === "true";
 
   if (faviconEl && faviconDarkExists) {
-    const lightFavicon = '/favicon.svg';
-    const darkFavicon = '/favicon-dark.svg';
+    const lightFavicon = '/en_noxsensor/favicon.svg';
+    const darkFavicon = '/en_noxsensor/favicon-dark.svg';
 
     const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
